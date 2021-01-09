73086ec9165f157b9afafd0e3d55419a4bf579cb
Julia Yacoub <48625439+jyacoub2@users.noreply.github.com>
2021-01-09 12:03:19 -0500

    Update answerRandomizer

--- Question.java
+++ Question.java
@@ -60,8 +60,28 @@ public class Question {
 	}
 	
 	// Randomly select answer order for the answers
-	public int[] answerRandomizer(int numAnswers) {
-			
-	}
+	public static int[] answerRandomizer(int numAnswers) {
+		//initialize Random class
+         	Random rand = new Random();
+         
+		//initialize array to return
+		int[] returnArray = new int[numAnswers];
 
+		//fill array with values 0 to numAnswers
+		for (int i=0; i<numAnswers; i++)
+		{
+		   returnArray[i]=i;
+		}
+
+		//iterate through all indices
+		for (int i=0; i<numAnswers; i++)
+		{
+		   int randIndex = rand.nextInt(numAnswers); //get a random number within the range of numAnswers
+		   int temp = returnArray[randIndex]; //set temp value to value of array at the random index
+		   returnArray[randIndex]= returnArray[i]; //change the array of randIndex to array[i]
+		   returnArray[i] = temp; //change array[i] to temp value
+		}
+		//System.out.println(Arrays.toString(returnArray));
+		return returnArray;
+	}
 }
