946a113eb9876721e255df3c0cfffe6d445e26d7
mpostma3 <44342123+mpostma3@users.noreply.github.com>
2021-01-10 07:08:39 -0500

    Update MapGUI.java

--- MapGUI.java
+++ MapGUI.java
@@ -1,6 +1,96 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
-public class MapGUI {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.concurrent.TimeUnit;
+
+public class MapGUI extends JFrame implements MouseListener {
 	
-	// ???
+	private Game play;
+	private int delaySeconds;
+	
+	private String imgMap = "Map.jpg";
+	private String selected;
+	
+	private ImageIcon map;
+	private JLabel mapLabel;
+	private JPanel panel;
+	
+	public MapGUI(Game play) {
+		
+		
+		delaySeconds = 5;
+		this.play = play;
+		
+		map = new ImageIcon(imgMap);
+		mapLabel = new JLabel(map);
+		mapLabel.setBounds(0, 0, 935, 700);
+		
+		panel = new JPanel();
+		panel.add(mapLabel);
+		panel.addMouseListener(this);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("EduPal");
+		this.setSize(935, 740);
+		this.add(panel);
+		this.setVisible(true);
+	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// Not required
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// Not required
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		AnswerGUI ansGUI;
+		Question nextQuestion;
+		int[] ansRandomize;
+		String[] answers;
+		
+		nextQuestion = play.questionRandomizer(1);
+		answers = new String[nextQuestion.getAnswers().length];
+		ansRandomize = nextQuestion.answerRandomizer(nextQuestion.getAnswers().length);
+		for (int j=0; j<answers.length; j++) {
+			answers[j] = nextQuestion.getAnswers()[ansRandomize[j]];
+		}
+		ansGUI = new AnswerGUI(nextQuestion.getStatement(), answers);
+		/*try {
+			TimeUnit.SECONDS.sleep(delaySeconds);
+		} catch (InterruptedException f) {
+			System.out.println("System interrupted...");
+		}*/
+		/*while (ansGUI.status == false) {
+			
+		}*/
+		selected = ansGUI.getSelected();
+		System.out.println("From MapGUI: " + selected);
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// Not required
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// Not required
+		
+	}
+	
+	public String getSelected() {
+		return selected;
+	}
 }
